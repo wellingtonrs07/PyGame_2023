710b1dc3b8725ef4c4fe22351968f5ebecb95734
wellingtonrs07 <wr24349@gmail.com>
2023-08-01 10:51:37 -0300

    inicio tela jogo

--- tela_jogo.py
+++ tela_jogo.py
@@ -1,1 +1,21 @@
 # Imports e arquivos
+from config import largura, altura, fps, QUIT, GAME,Azul
+import pygame
+from assets import ASFALTO,NEW_GAME,load_assets
+from classes import Button
+
+# Gera a tela
+tela = pygame.display.set_mode((largura, altura))
+
+def telajogo(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    assets = load_assets()[0]
+    btns = load_assets()[1]
+
+    telajogo = assets[as]
+    fundo_rect = telajogo.get_rect()
+
+    # começa a rodar o loop
+    running = True
