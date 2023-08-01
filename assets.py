efb3ddf92cc8e4e30f276ddc982b51821a966044
Fernando-Jun-Saito-Marui <fernandojsm@al.insper.edu.br>
2023-08-01 09:42:17 -0300

    config e assets

--- assets.py
+++ assets.py
@@ -1,2 +1,11 @@
-def assets():
+import pygame
+import os
+from config import WIDTH, HEIGHT, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_NPC
+
+PAISAGEM = 'paisagem'
+CARRO_IMG = 'carro'
+
+
+
+def load_assets():
     assets = {}
