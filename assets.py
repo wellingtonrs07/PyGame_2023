c860dd2ac0c0beff05837c0e4957b21d178961c6
wellingtonrs07 <wr24349@gmail.com>
2023-08-01 10:20:03 -0300

    Tela inicial

--- assets.py
+++ assets.py
@@ -4,10 +4,14 @@ from config import largura, altura, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_
 
 PAISAGEM = 'paisagem'
 CARRO_IMG = 'carro'
+ASFALTO = 'asfalto'
 TelaI = 'Tela Inicial'
 
 def load_assets():
     assets = {}
 
-    assets[TelaI] = pygame.image.load(os.path.join(Imagens, 'fundo_teste.png')).convert()
+    assets[TelaI] = pygame.image.load(os.path.join(Imagens, 'tela_inicial.png')).convert()
+    assets[TelaI] = pygame.transform.scale(assets[TelaI],(largura, altura))
+    assets[ASFALTO] = pygame.image.load(os.path.join(Imagens, 'asphalt-texture.png')).convert()
+
     return [assets]
