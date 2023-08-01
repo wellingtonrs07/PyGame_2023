78c33c33ad15f21b0a6c77f59331bc8ee71fb68b
wellingtonrs07 <wr24349@gmail.com>
2023-08-01 11:07:07 -0300

    assets arrumado

--- assets.py
+++ assets.py
@@ -4,7 +4,7 @@ from config import largura, altura, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_
 
 PAISAGEM = 'paisagem'
 CARRO_IMG = 'carro'
-RUA = 'asfalto'
+RUA = 'rua'
 TelaI = 'Tela Inicial'
 NEW_GAME = 'new_game'
 
@@ -14,7 +14,7 @@ def load_assets():
 
     assets[TelaI] = pygame.image.load(os.path.join(Imagens, 'menu.png')).convert()
     assets[TelaI] = pygame.transform.scale(assets[TelaI],(largura, altura))
-    assets[ASFALTO] = pygame.image.load(os.path.join(Imagens, 'asphalt-texture.png')).convert()
+    assets[RUA] = pygame.image.load(os.path.join(Imagens, 'rua.png')).convert()
 
     btns = {}
 
