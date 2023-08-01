1d291b3ee906e7765912436a0a4be5d30ee4f7f9
SidBarbosa <sidneybo@al.insper.edu.br>
2023-08-01 10:21:13 -0300

    assets

--- assets.py
+++ assets.py
@@ -4,10 +4,17 @@ from config import largura, altura, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_
 
 PAISAGEM = 'paisagem'
 CARRO_IMG = 'carro'
+
 TelaI = 'Tela Inicial'
+NEW_GAME = 'new_game'
 
 def load_assets():
     assets = {}
 
     assets[TelaI] = pygame.image.load(os.path.join(Imagens, 'fundo_teste.png')).convert()
-    return [assets]
+    assets[TelaI] = pygame.transform.scale(assets[TelaI], (largura, altura))
+
+    btns = {}
+
+    btns[NEW_GAME] = pygame.image.load(os.path.join(Imagens, 'botao_jogar')).convert()
+    return [assets,btns]
