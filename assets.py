d819a0aa2302af04e4dd226633ba638b970b7268
Fernando-Jun-Saito-Marui <fernandojsm@al.insper.edu.br>
2023-08-01 09:47:24 -0300

    assets e config arrumados

--- assets.py
+++ assets.py
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
 import pygame
 import os
-from config import WIDTH, HEIGHT, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_NPC
+from config import WIDTH, HEIGHT, WIDTH_CARRO, HEIGHT_CARRO, WIDTH_NPC,HEIGHT_NPC, Imagens
 
 PAISAGEM = 'paisagem'
 CARRO_IMG = 'carro'
-
-
+TelaI = 'Tela Inicial'
 
 def load_assets():
     assets = {}
-=======
-# Imagens, Sons, Fontes, Entre outros
-
-import pygame
-import os
-from config import largura, altura, Imagens
-
-# Cria as keys do dicionario
-
-TelaI = 'Tela Inicial'
-
-def assets():
-    assets = {}
 
     assets[TelaI] = pygame.image.load(os.path.join(Imagens, 'fundo_teste.png')).convert()
-
     return [assets]
->>>>>>> 083961e458c2bc20c420ac05914cd67b4ec93f23
--- config.py
+++ config.py
@@ -13,16 +13,10 @@ WIDTH_NPC = 100
 HEIGHT_NPC = 50
 
 # States
-<<<<<<< HEAD
 QUIT = 0
 GAME = 1
 INIT = 2
 END = 3
 
-=======
-quit = 0
-iniciando = 1
-
 # Paths para arquivos/pastas
 Imagens = path.join(path.dirname(__file__), 'images')
->>>>>>> 083961e458c2bc20c420ac05914cd67b4ec93f23
